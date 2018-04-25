789223e5f442d4b4d88ceeb3cb6d2cbd50310ae0
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-04-25 12:18:00 +0200

    sessions and csrf protection

---
+++ models/user.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+
+var schema = mongoose.Schema({
+    email: {type: String, required: true},
+    password: {type: String, required:true}
+});
+
+module.exports = mongoose.model("User", schema);
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
+var csurf = require('csurf');
+
+var csrfProtection = csurf();
+router.use(csrfProtection);
 
 /* GET Home Shop page. */
 router.get('/', function (req, res, next) {
@@ -12,6 +16,14 @@ router.get('/', function (req, res, next) {
         }
         res.render('shop/index', {products: productsChunk});
     });
+});
+
+router.get('/user/signup', (req,res,next)=> {
+    res.render('user/signup', {csrfToken: req.csrfToken()})
+});
+
+router.post('/user/signup', (req,res,next)=> {
+   res.redirect('/');
 });
 
 module.exports = router;

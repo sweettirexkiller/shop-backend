7fa306afde0e76bdd6c3a0fdbe7671b6ee62880e
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-04-25 11:33:05 +0200

    readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # shop-backend
+
+![shop](screem.png)
+
+This project is backend oriented ;) Please don't judge me on this Bootstrap (I hate bootstrap too).
+
+1) `git clone https://github.com/sweettirexkiller/shop-backend.git`
+2) `cd shop-backend`
+3) `npm install`
+4) `nano app.js`
+
+        ...
+        mongoose.connect('mongodb://localhost/YOUR_DB_HERE', {promiseLibrary: require('bluebird')});
+        ...
+        
+    `ctrl+O ctrl+X`
+ 
+5) `node seed/product-seed.js`
+6) `npm start`
+7) [localhost:3000](http://localhost:3000)
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Hello there!' });
+/* GET Home Shop page. */
+router.get('/', function (req, res, next) {
+    Product.find().then(docs => {
+        productsChunk = [];
+        const chunkSize = 3;
+        for (let i =0; i < docs.length; i+=chunkSize){
+            productsChunk.push(docs.slice(i,i+chunkSize));
+        }
+        res.render('shop/index', {products: productsChunk});
+    });
 });
 
 module.exports = router;

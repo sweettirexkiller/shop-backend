27344d40818dc588a73418b377257894e562fd84
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-04-27 17:31:58 +0200

    cart stored in cookie:'

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Product = require('../models/product');
 const csurf = require('csurf');
 const passport = require('passport');
+const Cart = require('../models/cart');
 
 const csrfProtection = csurf();
 router.use(csrfProtection);
@@ -12,14 +13,28 @@ router.get('/', function (req, res, next) {
     Product.find().then(docs => {
         productsChunk = [];
         const chunkSize = 3;
-        for (let i =0; i < docs.length; i+=chunkSize){
-            productsChunk.push(docs.slice(i,i+chunkSize));
+        for (let i = 0; i < docs.length; i += chunkSize) {
+            productsChunk.push(docs.slice(i, i + chunkSize));
         }
         res.render('shop/index', {products: productsChunk});
     });
 });
 
-
+router.get('/add-to-cart/:id', (req, res, next) => {
+    const productId = req.params.id;
+    let cart = new Cart(req.session.cart ? req.session.cart : {});
+    Product.findById(productId)
+        .exec()
+        .then(product=>{
+            cart.add(product,product._id);
+            req.session.cart = cart;
+            console.log(req.session.cart);
+            res.redirect('/');
+        })
+        .catch(err => {
+            res.redirect('/');
+        });
+});
 
 
 module.exports = router;

c9dc7ea0495b2929cab8e3ee227ee1245b55b288
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-04-30 12:54:06 +0200

    viewing shopping cart

--- routes/index.js
+++ routes/index.js
@@ -36,5 +36,13 @@ router.get('/add-to-cart/:id', (req, res, next) => {
         });
 });
 
+router.get('/shopping-cart', function (req, res, next) {
+   if(!req.session.cart){
+       return res.render('shop/shopping-cart', {products: null})
+   }
+   let cart = new Cart(req.session.cart);
+   res.render('shop/shopping-cart', {products: cart.generateArray(),totalPrice: cart.totalPrice})
+});
+
 
 module.exports = router;
